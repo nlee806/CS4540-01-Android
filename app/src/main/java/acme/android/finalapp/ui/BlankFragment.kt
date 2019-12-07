2ae23d46e444dcbce28bbcb162ad229d3b50ad78
alchew2000@gmail.com <achew5@calstatela.edu>
2019-12-07 14:43:25 -0800

    I added a blank fragment.

--- app/src/main/java/acme/android/finalapp/ui/BlankFragment.kt
+++ app/src/main/java/acme/android/finalapp/ui/BlankFragment.kt
@@ -22,7 +22,9 @@ private const val ARG_PARAM2 = "param2"
  * to handle interaction events.
  * Use the [BlankFragment.newInstance] factory method to
  * create an instance of this fragment.
+ *
  */
+
 class BlankFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
